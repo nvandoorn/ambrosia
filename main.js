2a4666d2156af910bd59dd04413e16d64d999818
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-03-18 23:20:11 -0700

    Add comments & pull root out of function

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+// Use the "Document Object Model" API to fetch
+// our "root" node as defined in index.html
+let root = document.getElementById("root");
+
+// Define an "asyncrhonous" function (i.e we need to "await" it)
+// to "draw" the market price onto our UI
 async function drawUI() {
-  let root = document.getElementById("root");
+  // consult the coinpaprika API for better info
+  // on these objects
   let markets = await cp.markets();
   let stockData = markets[0].quotes.USD.price;
-  root.innerHTML = `<h1> BTC USD ${stockData}</ h1>`;
+  // set the `innerHTML` property of our root node
+  root.innerHTML = `<h1>BTC USD ${stockData}</h1>`;
 }
 
+// initial draw
 drawUI();
 
+// redraw every 2000ms
 setInterval(drawUI, 2000);
